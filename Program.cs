f316350baa5677f45cd5062958b87b4921110339
Aaron <72924198+AaronCrvl@users.noreply.github.com>
2025-04-22 09:51:57 -0300

    String de Conexão Azure

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-  options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection"))
+  options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDB"))
 );
 
 // Add services to the container.
